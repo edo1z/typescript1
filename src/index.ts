3b35b152d8ce03af7cd2ffbe7d0889899b6c4e2e
web3ten0 <web3ten0@gmail.com>
2021-12-17 13:32:22 +0900

    generics test

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
-const x = 12
-const a = (x: number) :number => x + 10
-console.log(a(x))
-
-const b: boolean = false
-const c: string = 'hoge'
-const d: number[] = [1,2,3,4]
-const e: [string, number] = ['hoge', 30]
-const f: any = 'hoge'
-const g: unknown = 'hoge'
-const h = (y: number) :void => {console.log(y)}
-h(120)
+type a<T> = {
+    x: T,
+    y?: number
+}
+const a1:a<string> = {x:'hoge', y:12};
+function b<T>(arg: T): T {
+    return arg;
+}
+const a2 = b(12);
+console.log(a2);
